8961e8702b0adcf9a8f22c6bf445833d45471f9d
Anthony <sean.a.protho1@gmail.com>
2021-02-04 10:37:52 -0800

    refactored inline method

--- inline_method.py
+++ inline_method.py
@@ -1,25 +1,14 @@
-# by Kami Bigdely
-# Inline method.
-# TODO: Refactor this program to improve its readability.
-
-LEGAL_DRINKING_AGE = 18
+drinking_age = 18
 class Person:
     def __init__(self, my_age):
         self.age = my_age
         
 def enter_night_club(individual):
-    if older_than_18_year_old(individual.age):
+    if individual.age >= drinking_age:
         print("Allowed to enter.")
-    else:
-        print("Enterance of minors is denited.")
-
-def older_than_18_year_old(age):
-    if age > LEGAL_DRINKING_AGE:
-        return True
     else: 
-        return False
+        print("Entrance of minors is denied.")
     
     
 person = Person(17.9)
 enter_night_club(person)
-        
