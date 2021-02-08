b0334c64bf0b5e9021620b82bfcdc9e2148e2b18
Anthony <sean.a.protho1@gmail.com>
2021-02-07 22:18:43 -0800

    extract method

--- extract_method.py
+++ extract_method.py
@@ -1,28 +1,46 @@
+"""This module prints students statistics"""
 # Written by Kamran Bigdely
 # Example for Compose Methods: Extract Method.
-import math 
+import math
 
-def print_stat():
-    grade_list = []
-    # Get the inputs from the user
-    n_student = 5
-    for _ in range(0, n_student):
-        grade_list.append(int(input('Enter a number: ')))
+class StudentStatistics():
+    """Class that runs all our statistics"""
+    def __init__(self):
+        self.grade_list = []
+        self.total = 0
+        self.mean = 0
 
-    # Calculate the mean and standard deviation of the grades
-    sum = 0 # Do you think 'sum' is a good var name? Run pylint to figure out!
-    for grade in grade_list:
-        sum = sum + grade
-    mean = sum / len(grade_list)
-    sd = 0 # standard deviation
-    sum_of_sqrs = 0
-    for grade in grade_list:
-        sum_of_sqrs += (grade - mean) ** 2
-    sd = math.sqrt(sum_of_sqrs / len(grade_list))
-    # print out the mean and standard deviation in a nice format.
-    print('****** Grade Statistics ******')
-    print("The grades's mean is:", mean)
-    print('The population standard deviation of grades is: ', round(sd, 3))
-    print('****** END ******')
+    def s_d(self):
+        """Calculates standard deviation of the grades"""
+        sum_of_squares = 0
+        for grade in self.grade_list:
+            sum_of_squares += (grade - self.mean) ** 2
+        return math.sqrt(sum_of_squares / len(self.grade_list))
 
-print_stat()
+    def get_mean(self):
+        """Calculates mean and total of the grades"""
+        for grade in self.grade_list:
+            self.total = self.total + grade
+        self.mean = self.total / len(self.grade_list)
+
+    def get_input(self):
+        """Collects and returns input for n students aka grade_list"""
+        n_student = 5
+        for _ in range(0, n_student):
+            self.grade_list.append(int(input('Enter a number: ')))
+
+    def print_stat(self):
+        """prints out statistics of n_students grades"""
+        # Get the statistics
+        self.get_input()
+        self.get_mean()
+        mean = self.mean
+        s_d = self.s_d()
+        # print out the mean and standard deviation in a nice format.
+        print('****** Grade Statistics ******')
+        print("The grades's mean is:", mean)
+        print('The population standard deviation of grades is: ', round(s_d, 3))
+        print('****** END ******')
+
+s = StudentStatistics()
+s.print_stat()
